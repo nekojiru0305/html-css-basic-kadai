87c5f79b79c4ce9f50433167165a02859ac4a1a2
nekojiru0305 <143889371+nekojiru0305@users.noreply.github.com>
2024-01-07 20:29:11 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,11 @@
+let num = 25;
+
+ if ( num % 3 === 0 ){
+   console.log("3の倍数である");
+} else if ( num % 5 === 0 ){
+    console.log("5の倍数である");
+} else if ( num % 3 === 0 && num % 5 === 0 ){
+   console.log("3と5の倍数です");
+} else {
+    console.log("num");  
+}
