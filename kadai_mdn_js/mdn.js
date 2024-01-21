a78581b2282cad8bb5c28eba3bfb359ce338c246
nekojiru0305 <143889371+nekojiru0305@users.noreply.github.com>
2024-01-21 14:54:12 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,13 @@
+const date = new Date();
+
+const options = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
+};
+
+const formattedDate = date.toLocaleDateString('ja-JP', options);
+console.log(formattedDate);
+
+
+
