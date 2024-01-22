d24dd2ce0abe9d44b815ecafcdc5e8c895ee1c4f
nekojiru0305 <143889371+nekojiru0305@users.noreply.github.com>
2024-01-22 22:32:09 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,16 @@
+// 与えられた引数priceに送料を加算し、その値を出力する関数を定義する
+const calculateTotal = (price) => {
+    console.log(price + 50);
+  }
+  
+  // 関数を呼び出し、引数として購入金額を渡す
+  calculateTotal(50);
+ 
+
+  // 与えられた引数numを2倍にし、その値を戻り値として返す関数を定義する
+ const double = (num) => {
+    return num * 2;
+  }
+  
+  // 関数の戻り値を出力する
+  console.log(double(50));
